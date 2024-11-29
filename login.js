9e88dc3cd9d1b4dcfd3bfb2f461f46a5262ab63f
HarrisonETITC <127680516+HarrisonETITC@users.noreply.github.com>
2024-11-29 00:47:20 -0500

    Ajustada altura para celulares y cambio de mensaje de pop up cuando el usuario ya está registrado

--- login.js
+++ login.js
@@ -1,22 +1,25 @@
 document.body.onload = addElement;
 
+let h3Mensaje = {};
+
 function addElement() {
     // create a new div element 
     // and give it popup content 
     const newDiv = document.createElement("div");
     newDiv.classList.add('position-absolute', 'z-3')
-    newDiv.setAttribute('id','popup');
+    newDiv.setAttribute('id', 'popup');
     newDiv.style.display = 'none';
     newDiv.style.width = '100vw';
     newDiv.style.height = '100vh';
     newDiv.style.alignItems = 'center';
     newDiv.style.justifyContent = 'center';
     newDiv.style.backgroundColor = 'rgb(0 0 0 / .3)';
-    newDiv.innerHTML += '<div class="card shadow-lg p-4"><h3 class="mb-5">¿Desea ayudarnos completando un pequeño formulario? Este no le tomará mucho tiempo :)</h3> <div class="d-flex gap-3"> <button type="button" class="btn btn-primary w-100 fw-bold fs-4" onclick="irDemografica()">De acuerdo!</button> <button type="button" class="btn btn-danger w-100 fw-bold fs-4" onclick="irGracias()">Tal vez después</button> </div> </div>';
+    newDiv.innerHTML += `<div class="card shadow-lg p-4"><h3 id="popUpMensaje" class="mb-5"></h3> <div class="d-flex gap-3"> <button type="button" class="btn btn-primary w-100 fw-bold fs-4" onclick="irDemografica()">De acuerdo!</button> <button type="button" class="btn btn-danger w-100 fw-bold fs-4" onclick="irGracias()">Tal vez después</button> </div> </div>`;
 
     // add the newly created element and its content into the DOM 
     const currentDiv = document.getElementById("main_container");
     document.body.insertBefore(newDiv, currentDiv);
+    h3Mensaje = document.getElementById('popUpMensaje');
 }
 
 function openPopup() {
@@ -47,7 +50,12 @@ function login() {
     })
         .then(response => response.json())
         .then(body => {
-            if (body.message === 'Usuario creado correctamente') {
+            if (body.message == 'El usuario ya se encuentra registrado') {
+                h3Mensaje.innerText = 'Hemos notado que ya se encuentra registrado en nuestro sistema. ¿Desea corroborar su información demográfica? :)';
+                localStorage.setItem('numero', numero);
+                openPopup();
+            } else if (body.message === 'Usuario creado correctamente') {
+                h3Mensaje.innerText = '¿Desea ayudarnos completando un pequeño formulario? Este no le tomará mucho tiempo :)';
                 localStorage.setItem('numero', numero);
                 openPopup();
             } else {
